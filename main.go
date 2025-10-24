762fc054df82c6a22c3cb4ff74f38294173ff519
Pavel Baksheev <sxeiter@mail.ru>
2025-10-24 08:48:43 +0500

    написал базовый функционал, дальше больше

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	fmt.Println("Hello, go!")
+	const IMTPower = 2
+	var UserHeigth float64
+	var UserWeight float64
+	fmt.Println("__Калькулятор индекса массы тела__")
+	fmt.Println("Введите ваш рост в метрах")
+	fmt.Scan(&UserHeigth)
+	fmt.Println("Введите ваш вес")
+	fmt.Scan(&UserWeight)
+	IMT := UserWeight / math.Pow(UserHeigth, IMTPower)
+	fmt.Printf("Ваш индекс массы тела: %v", math.Round(IMT))
 }
