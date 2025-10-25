8b5c1ad5fdf8ad007b6b1472af7424e883d621a2
Pavel Baksheev <sxeiter@mail.ru>
2025-10-25 18:25:50 +0500

    добавил немного логики

--- main.go
+++ main.go
@@ -8,27 +8,57 @@ import (
 const IMTPower = 2
 
 func main() {
-	fmt.Println("__Калькулятор индекса массы тела__")
-	IMT := calculateIMT(getUserInput())
-	outputResult(IMT)
+
+	for {
+		fmt.Println("__Калькулятор индекса массы тела__")
+		userWeight, userHeight := getUserInput()
+		IMT := calculateIMT(userWeight, userHeight)
+		outputResult(IMT)
+		isRepeatCalculation := checkPereatCalculation()
+		if !isRepeatCalculation {
+			break
+		}
+	}
 }
 
 func outputResult(imt float64) {
 	result := fmt.Sprintf("Ваш индекс массы тела: %.0f", imt)
 	fmt.Println(result)
+	switch {
+	case imt < 16:
+		fmt.Println("У вас сильный дефицит массы тела")
+	case imt < 18.5:
+		fmt.Println("У вас дефицит массы тела")
+	case imt < 25:
+		fmt.Println("У вас нормальный вес")
+	case imt < 30:
+		fmt.Println("У вас избыточный вес")
+	default:
+		fmt.Println("У вас степень ожирения")
+	}
 }
 
 func calculateIMT(userWeigth, userHeigth float64) float64 {
-	IMT := userWeigth / math.Pow(userHeigth/100, IMTPower)
+	IMT := userHeigth / math.Pow(userWeigth/100, IMTPower)
 	return IMT
 }
 
 func getUserInput() (float64, float64) {
-	var userHeigth float64
 	var userWeight float64
+	var userHeigth float64
 	fmt.Println("Введите ваш рост в сантиметрах")
 	fmt.Scan(&userHeigth)
 	fmt.Println("Введите ваш вес")
 	fmt.Scan(&userWeight)
 	return userHeigth, userWeight
+}
+
+func checkPereatCalculation() bool {
+	var userChoise string
+	fmt.Println("Хотите сделать расчет еще раз. (Y/N) ")
+	fmt.Scan(&userChoise)
+	if userChoise == "y" || userChoise == "Y" {
+		return true
+	}
+	return false
 }
